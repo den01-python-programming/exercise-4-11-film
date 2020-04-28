38fd34e61466f126d46c3e0c6b2a6a2d27007f30
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:54:59 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,15 @@
 import pytest
-import src.exercise
+from src.film import Film
 
-def test_exercise():
-    #implement tests here
-    assert 0 == 0
+def test_exercise(capsys):
+    chipmunks = Film("Alvin and the Chipmunks: The Squeakquel", 0)
+    age = 20
+
+    print()
+    if (age >= chipmunks.age_rating()):
+        print("You may watch the film " + chipmunks.name())
+    else:
+        print("You may not watch the film " + chipmunks.name())
+
+    out, err = capsys.readouterr()
+    assert out == "You may watch the film Alvin and the Chipmunks: The Squeakquel\n"
