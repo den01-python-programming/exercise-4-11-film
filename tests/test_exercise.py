e730cdd8d4ab8c9401b849a8c1c6d3c81f917a7c
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 12:40:38 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -5,11 +5,10 @@ def test_exercise(capsys):
     chipmunks = Film("Alvin and the Chipmunks: The Squeakquel", 0)
     age = 20
 
-    print()
-    if (age >= chipmunks.age_rating()):
-        print("You may watch the film " + chipmunks.name())
+    if (age >= chipmunks.age_rating):
+        print("You may watch the film " + chipmunks.name)
     else:
-        print("You may not watch the film " + chipmunks.name())
+        print("You may not watch the film " + chipmunks.name)
 
     out, err = capsys.readouterr()
     assert out == "You may watch the film Alvin and the Chipmunks: The Squeakquel\n"
